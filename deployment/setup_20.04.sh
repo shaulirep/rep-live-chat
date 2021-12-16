46e06f041edc403fb137cf801087dc13a8a67b15
shaulirep <95334569+shaulirep@users.noreply.github.com>
2021-12-16 02:59:38 +0200

    Update setup_20.04.sh
    changed the installation script to look at the new repo

--- deployment/setup_20.04.sh
+++ deployment/setup_20.04.sh
@@ -87,7 +87,7 @@ rvm autolibs disable
 rvm install "ruby-3.0.2"
 rvm use 3.0.2 --default
 
-git clone https://github.com/chatwoot/chatwoot.git
+git clone https://github.com/shaulirep/rep-live-chat.git
 cd chatwoot
 if [[ -z "$1" ]]; then
 git checkout master;
@@ -136,7 +136,7 @@ echo "To configure a domain and SSL certificate, follow the guide at https://www
 echo "***************************************************************************"
 else
 curl https://ssl-config.mozilla.org/ffdhe4096.txt >> /etc/ssl/dhparam
-wget https://raw.githubusercontent.com/chatwoot/chatwoot/develop/deployment/nginx_chatwoot.conf
+wget https://raw.githubusercontent.com/shaulirep/rep-live-chat/develop/deployment/nginx_chatwoot.conf
 cp nginx_chatwoot.conf /etc/nginx/sites-available/nginx_chatwoot.conf
 certbot certonly --nginx -d $domain_name
 sed -i "s/chatwoot.domain.com/$domain_name/g" /etc/nginx/sites-available/nginx_chatwoot.conf
