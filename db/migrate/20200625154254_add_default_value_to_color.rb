887d1aa8cb62e6c206118f2f429bec94db6d98bd
Vitaliy Goncharov <vitaliy.goncharov@a-listware.com>
2021-12-23 07:50:39 +0200

    VCAD-4804 #comment deprecate blue, favicon updated

--- db/migrate/20200625154254_add_default_value_to_color.rb
+++ db/migrate/20200625154254_add_default_value_to_color.rb
@@ -1,8 +1,8 @@
 class AddDefaultValueToColor < ActiveRecord::Migration[6.0]
   def up
-    Label.where(color: nil).find_each { |u| u.update(color: '#1f93ff') }
+    Label.where(color: nil).find_each { |u| u.update(color: '#3B3A8F') }
 
-    change_column :labels, :color, :string, default: '#1f93ff', null: false
+    change_column :labels, :color, :string, default: '#3B3A8F', null: false
   end
 
   def down
